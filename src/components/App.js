6649f64d09fb670d4177f2784d7eaf63c34f143e
Ryan Granger <ryan.granger98@gmail.com>
2023-11-07 18:29:48 +0000

    first commit of branch

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/App.css";
 
 const App = () => {
-  return <div className="App" />;
+  return <div className="App">Train Punctuality App</div>;
 };
 
 export default App;
