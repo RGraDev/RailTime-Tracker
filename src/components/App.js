7f40d9f664c7b9d2e320386182f91e5238a23a6c
Wasiu-hub <contactbow@yahoo.com>
2023-11-09 03:03:20 +0000

    initial commit updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from "react";
-import "../styles/App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
-  return <div className="App " />;
+  return (
+    <div className="App ">
+      <p>
+        This App assists in creating an effective travel itinerary by providing
+        information on the punctuality of train services.
+      </p>
+
+      <Router>
+        <Routes />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
